7f959a837c543e7ec83f1d4a51bc822662ce9192
amanda1686 <amanda160986@gmail.com>
2024-02-14 13:51:21 +0100

    dashboard_private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import PrivateRoute from './components/PrivateRoute'
 
 
 export default function App() {
@@ -17,7 +18,9 @@ export default function App() {
 <Routes>
   <Route path='/' element={<Home />}/>
   <Route path='/about' element={<About />}/>
-  <Route path='/dashboard' element={<Dashboard />} />
+  <Route element={<PrivateRoute/>}>
+   <Route path='/dashboard' element={<Dashboard />} /> 
+  </Route>
   <Route path='/proyects' element={<Proyects />}/>
   <Route path='/signin' element={<SignIn/>} />
   <Route path='/signup' element={<SignUp/>}/>
