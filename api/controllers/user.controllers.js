d426caa6704303db30e0e19b788fe696885e6179
amanda1686 <amanda160986@gmail.com>
2024-02-15 14:44:30 +0100

    detele_user_api-route

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -46,3 +46,16 @@ export const updateUser = async (req, res, next) => {
         }
 
 };
+
+export const deleteUser = async (req, res, next) =>{
+    if (req.user.id !== req.params.userId) {
+        return next(errorHandler(403, 'You are not allowedto delete '));
+    }
+    try  {
+        await User.findByIdAndDelete(req.params.userId);
+        res.status(200).json('User has been deleted');
+    } catch (error) {
+        next(error);
+    }
+
+};
