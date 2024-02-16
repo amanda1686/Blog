47d0aa4a91bde65fe967aff60a63418e50e65fec
amanda1686 <amanda160986@gmail.com>
2024-02-16 16:06:11 +0100

    create post route

--- api/controllers/post.controllers.js
+++ api/controllers/post.controllers.js
@@ -20,3 +20,47 @@ console.log(req.user);
         next(error);
     }
 } ;
+
+export const getpost = async (req, res, next) =>{
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 6;
+        const sortDirection = req.query.order === 'asc' ? 1 : -1;
+        
+        const post = await Post.find({
+            ...(req.query.userId && { userId: req.query.userId }),
+            ...(req.query.category && { category: req.query.category }),
+            ...(req.query.slug && { slug: req.query.slug }),
+            ...(req.query.postId && { _id: req.query.postId }), // Coma agregada aquí
+            ...(req.query.searchTerm && {
+                $or:[
+                    { title: { $regex: req.query.searchTerm, $options: 'i' } }, // Espacio eliminado después de 'i'
+                    { content: { $regex: req.query.searchTerm, $options: 'i' } } // Espacio eliminado después de 'i'
+                ]
+            })
+        }).sort({ update: sortDirection}).skip(startIndex).limit(limit);
+
+        const totalPost = await Post.countDocuments();
+
+        const now = new Date();
+
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() -1,
+            now.getDate()
+        );
+
+        const lastMonthPost = await Post.countDocuments({
+            createdAt: {$gte: oneMonthAgo},
+        });
+
+        res.status(200).json({
+            post,
+            totalPost,
+            lastMonthPost,
+        });
+
+    } catch (error) {
+        next(error)
+    }
+}
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js'
-import { create } from '../controllers/post.controllers.js';
+import { create, getpost } from '../controllers/post.controllers.js';
 
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create)
+router.get('/getpost', getpost)
 
 export default router;
