11bf0625d332fc2451ac14c58a75875b9098c54c
amanda1686 <amanda160986@gmail.com>
2024-02-17 23:47:26 +0100

    delete user-admin

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -49,9 +49,22 @@ export default function DashUsers() {
             console.log(error.message);
         }
     }
-const handleDeleteUser = async () => {
-
-}
+    const handleDeleteUser = async () => {
+        try {
+            const res = await fetch(`/api/user/delete/${userIdToDelete}`, {
+                method: 'DELETE',
+            });
+            const data = await res.json();
+            if (res.ok) {
+                setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete));
+                setShowModal(false);
+            } else {
+                console.log(data.message);
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+      };
 
 
   return (
