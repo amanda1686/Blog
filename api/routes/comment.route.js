e4139f447d947282d506c86fa233aa500386a0a9
amanda1686 <amanda160986@gmail.com>
2024-02-18 18:38:14 +0100

    add comment section

---
+++ api/routes/comment.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { createComment } from "../controllers/comment.controllers.js";
+import { verifyToken } from '../utils/verifyUser.js'
+
+const router = express.Router();
+
+router.post('/create', verifyToken, createComment)
+
+export default router;
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@ import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
+import CommentSection from "../components/CommentSection";
 
 export default function PostPage() {
     const { postSlug } = useParams();
@@ -65,6 +66,7 @@ if (loading) {
             <div className=" max-w-4xl mx-auto w-full">
                 <CallToAction/>
             </div>
+            <CommentSection postId={post._id}/>
         </div>
     );
 }
