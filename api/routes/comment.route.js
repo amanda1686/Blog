d700eac7cee0c00219ad1c2c4a4677b56f7c373a
amanda1686 <amanda160986@gmail.com>
2024-02-18 23:05:02 +0100

    add like funtionality

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createComment, getPostComments } from "../controllers/comment.controllers.js";
+import { createComment, getPostComments,likeComment } from "../controllers/comment.controllers.js";
 import { verifyToken } from '../utils/verifyUser.js'
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
+router.put('/likeComment/:commentId', verifyToken, likeComment);
 
 export default router;
--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,10 +1,12 @@
 import moment from 'moment';
 import React, { useEffect, useState } from 'react';
+import { IoBookSharp } from "react-icons/io5";
+import { useSelector } from 'react-redux';
 
-export default function Comment({ comment }) {
+
+export default function Comment({ comment, onLike }) {
   const [user, setUser] = useState({});
-  console.log(user);
-  
+  const {currentUser} = useSelector((state) => state.user);
   useEffect(() => {
     const getUser = async () => {
       try {
@@ -37,6 +39,23 @@ export default function Comment({ comment }) {
                 <span className='text-gray-500 text-xs '>{moment(comment.createdAt).fromNow()}</span>
             </div>
             <p className='mb-2'>{comment.content}</p>
+            <div className=" flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
+            <button 
+              type='button' 
+              onClick={() => onLike(comment._id)} 
+              className={`text-gray-400 hover:text-pink-500 ${
+              currentUser && comment.likes.includes(currentUser._id) ? 'text-pink-500' : ''
+              }`}
+              >
+           <IoBookSharp className='text-sm'/>
+            </button>
+            <p className='text-gray-400'>
+              {
+               comment.numberOfLikes > 0 && comment.numberOfLikes + " " + (comment.numberOfLikes === 1 ? "like" : "likes")
+              }
+          </p>
+
+            </div>
         </div>
     </div>
   )
