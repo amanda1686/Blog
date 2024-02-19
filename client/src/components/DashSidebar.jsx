e898c47562da0ae0ef296e052e683b184298283b
amanda1686 <amanda160986@gmail.com>
2024-02-19 09:12:15 +0100

    add comments section

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import { Sidebar } from 'flowbite-react'
-import {HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser} from 'react-icons/hi'
+import {HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser} from 'react-icons/hi'
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userSlice';
@@ -60,7 +60,8 @@ export default function DashSidebar() {
                 </Link>
                 )}
                 {currentUser.isAdmin && (
-                <Link to='/dashboard?tab=users'>
+                  <>
+                  <Link to='/dashboard?tab=users'>
                   <Sidebar.Item
                     active={tab === 'users'}
                     icon={HiOutlineUserGroup}
@@ -69,6 +70,16 @@ export default function DashSidebar() {
                     Users
                   </Sidebar.Item>
                 </Link>
+                  <Link to='/dashboard?tab=comments'>
+                  <Sidebar.Item
+                    active={tab === 'users'}
+                    icon={HiAnnotation}
+                    as='div'
+                  >
+                    Comments
+                  </Sidebar.Item>
+                </Link>
+                  </>
                 )}
                 <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout} >
                     Sign Out
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import DashSidebar  from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile'
 import DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers';
+import DashComments from '../components/DashComments';
 
 
 export default function Dashboard() {
@@ -28,6 +29,8 @@ export default function Dashboard() {
         {tab === 'post' && <DashPosts/>}
         {/* users */}
         {tab === 'users' && <DashUsers/>}
+        {/* commets */}
+        {tab === 'comments' && <DashComments/>}
       
     </div>
   )
